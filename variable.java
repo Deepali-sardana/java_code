6e15a76addd58567a1a6554bcfa73b3c9aa88f83
Deepali-sardana <110257282+Deepali-sardana@users.noreply.github.com>
2023-07-28 18:40:18 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,7 @@
+public class variable {
+    public static void main(String[] args) {
+        int a=90;//declaring a variable of type int where a is variable or a container which holds the value
+        int b=98;
+        System.out.println(a+b);
+    }
+}
