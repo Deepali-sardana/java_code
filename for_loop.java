82e5de2f21c5cc9fc21564334b2bceedbf290b00
Deepali-sardana <110257282+Deepali-sardana@users.noreply.github.com>
2023-07-29 00:55:20 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class for_loop {
+    public static void main(String[]args){
+        for(int i=0;i<=5;i++)//in for loop we initialize the variable compare it and do update(inc/dec)
+        {
+            System.out.println(i);
+        }
+        //example of for loop to print first 10 odd number
+        for(int i=0;i<=10;i++){
+            System.out.println(2*i+1);
+        }
+    }
+}
+
