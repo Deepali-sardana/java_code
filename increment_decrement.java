a4c1f84b6ecad0f0fb1ffda19ab8d421745078b6
Deepali-sardana <110257282+Deepali-sardana@users.noreply.github.com>
2023-07-29 00:01:07 +0530

    Add files via upload

---
+++ increment_decrement.java
@@ -0,0 +1,16 @@
+package com.company;
+public class increment_decrement {
+    public static void main(String[]args){
+     int a=7;
+     int b=3;
+        System.out.println(a++);//it will use first then increment
+        System.out.println(a);//increment value
+        System.out.println(++a);//first increment the value then use it
+        System.out.println(b--);//it will use first then decrement
+        System.out.println(b);//decremented value
+        System.out.println(--b);//first decrement the value then use it
+       char ch='a';
+        System.out.println(++ch);
+
+    }
+}
