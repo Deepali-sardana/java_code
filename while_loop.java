04973dc431ead82f0806ea8ffc81ee0a8ae2706f
Deepali-sardana <110257282+Deepali-sardana@users.noreply.github.com>
2023-07-29 13:03:52 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class while_loop {
+    public static void main(String[]args){
+        int i=0;//initialization
+        while(i<=10)//condition
+        {
+            System.out.println(i);
+            i++;//update
+        }
+        //print first 10 even number
+        int j=0;
+        while (j<10){
+            System.out.println(2*j);
+            j++;
+        }
+    }
+}
