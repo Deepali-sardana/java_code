696d1f06cfd356ad8b3a5c3be605b0b5bdc4db33
Deepali-sardana <110257282+Deepali-sardana@users.noreply.github.com>
2023-07-29 13:12:36 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class do_while {
+    public static void main(String[]args){
+        int i=0;
+        do//it will execute at least one time whether the condition is true or not
+        {
+            System.out.println(i);
+            i++;
+        }while (i<=10);//semicolon is must
+    }
+}
